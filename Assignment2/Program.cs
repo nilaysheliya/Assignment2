123d2e1809f50e07faa1db9953f4bc4638d235ed
sheli <sheli@DESKTOP-UAHU11D>
2024-02-10 18:50:30 -0500

    Add project files.

---
+++ Assignment2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Position
+{
+    public int X 
+    { 
+        get; 
+        set; 
+    }
+    public int Y 
+    { 
+        get; 
+        set; 
+    }
+
+    // constructor
+    public Position(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+}
